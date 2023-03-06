54ddb1b89570eef9bc43bf2fa5127ddc8615abdb
Sophie Reed <sr525@sdfrome002.sdf.slac.stanford.edu>
2023-03-06 10:47:40 -0800

    Update objetTableSurveyAnalysis to use astropy rather than pandas

--- python/lsst/analysis/tools/tasks/objectTableSurveyAnalysis.py
+++ python/lsst/analysis/tools/tasks/objectTableSurveyAnalysis.py
@@ -30,6 +30,7 @@ if TYPE_CHECKING:
     from lsst.daf.butler import DataCoordinate, DeferredDatasetHandle
 
 from lsst.pipe.base import connectionTypes as ct
+from astropy.table import vstack
 
 from ..actions.plot.plotUtils import shorten_list
 from ..interfaces import KeyedData
@@ -51,7 +52,7 @@ class ObjectTableSurveyAnalysisConnections(
     data = ct.Input(
         doc="Input catalog of objects",
         name="objectTable_tract",
-        storageClass="DataFrame",
+        storageClass="ArrowAstropy",
         deferLoad=True,
         multiple=True,
         dimensions=("tract", "skymap"),
@@ -120,4 +121,7 @@ class ObjectTableSurveyAnalysisTask(AnalysisPipelineTask):
         if names is None:
             names = self.collectInputNames()
 
-        return cast(KeyedData, pd.concat(h.get(parameters={"columns": names}) for h in handle))
+        cats = []
+        for h in handle:
+            cats.append(h.get(parameters={"columns": names}))
+        return vstack(cats)
